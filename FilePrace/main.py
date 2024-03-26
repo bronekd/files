017ffb2e3cf26eea2a7790ee7f42f822c75cbcbb
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-03-26 19:03:50 +0100

    První práce se soubory otevřít zavřít a vložení prvního textu do souboru. Je možné mít různé přípony souboru.

--- FilePrace/main.py
+++ FilePrace/main.py
@@ -1,5 +1,21 @@
-print("Hello World")
+# soubory
+# ukládání změn v aplikací ukládám data. Teď bude jak uložit data a načíst data.
+# nezáleží na koncovce nemusí být txt ale třeba me, můžu si vytvořit valstní typ přípony
 
-print("Hello World 2")
+file = open("first_file.me","w")
+file.write("Test text")
+file.close()
 
-# a funguje mi komentář i zde ???
+# Při čtení musí soubor existovat
+file = open("first_file.me", "r")
+data = file.read()
+file.close()
+print(data)
+
+
+file = open("first_file.me","a")
+file.write("\ndruhy radek")
+file.close()
+
+
+
