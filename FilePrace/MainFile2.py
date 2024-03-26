423c352a53948d77b610635427bb8d0af186c969
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-03-26 20:08:44 +0100

    MOje řešení okolu taks 1

--- FilePrace/MainFile2.py
+++ FilePrace/MainFile2.py
@@ -1,6 +1,34 @@
-print("Hello World 2")
-# toto je jen přidání komentáře
+# Task 1
+# otevřít soubor a načíst
+# rozsekat data po mezeru pomocí len a pokud je to 7 a víc tak uložím do nového seznamu
+
+#grafická okna přes knihovnu udělá exe soubor. qt python, pygame
+
+"""
+# Moje řešení:
+
+file = open("first_task.txt","w")
+file.write("Test text ahoj Jmenotrs asdfgtrsf")
+file.close()
+
+file = open("first_task.txt", "r")
+data = file.read()
+file.close()
+data.split(" ")
+
+my_world = []
+for i in data.split(" "):
+    if len(i) > 7:
+        my_world.append(i + " ")
 
 
+file = open("first_tas_outputs.txt", "w")
+file.writelines(my_world)
+file.close()
 
+print(my_world)
+print()
+"""
+# řešení učitel
 
+def task1(source, output_name):
