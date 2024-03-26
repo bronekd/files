aaef7714c50e15f1a0e558e208d7a8626e87e543
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-03-26 21:03:55 +0100

    Další úkol task 3 a jeho implementace

---
+++ FilePrace/Task3.py
@@ -0,0 +1,26 @@
+# task 3
+# You have a text file. Write its lines to another file. The order of lines in the second file must be inverse.
+# Máte textový soubor. Zapište jeho řádky do jiného souboru. Pořadí řádků ve druhém souboru musí být inverzní.
+import re
+
+def task3(source_name, output_name):
+    #načíst
+    f_handler = open("TextTask3.txt", "r")
+    data = f_handler.readlines()
+    f_handler.close()
+
+    f_handler = open(output_name, "w")
+    f_handler.close()
+
+    #uprava poslednich dat a mezere
+    if data[-1][-1] != "\n":
+        data[-1] = data[-1] + "\n"
+
+    #prohodit
+    f_handler = open(output_name, "a")
+    for i in range(len(data) - 1, -1, -1):
+        f_handler.write(data[i])
+    f_handler.close()
+
+task3("TextTask3.txt", "OutputTask3.txt")
+#OutputTask3.txt
